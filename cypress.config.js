f95876b75cb4cc2a814b6b536b155b61e0f65a52
jackt24 <48061691+jackt24@users.noreply.github.com>
2024-05-30 17:11:36 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "5zo58p",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  
+});
